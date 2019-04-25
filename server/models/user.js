989abb8136949f5ea305f22134a115186093371c
Richard Eldridge <eldridge.rich@gmail.com>
2019-04-25 10:28:57 -0400

    add test route

--- server/models/user.js
+++ server/models/user.js
@@ -1,29 +1,30 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-var crypto = require('crypto');
-var jwt = require('jsonwebtoken');
+
+const { Schema } = mongoose;
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 // Define our model
 const UserSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   hash: String,
   salt: String
-})
+});
 
-UserSchema.methods.setPassword = function(password){
+UserSchema.methods.setPassword = function(password) {
   this.salt = crypto.randomBytes(16).toString('hex');
 
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
-}
+};
 
 UserSchema.methods.validPassword = function(password) {
-  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
 
   return this.hash === hash;
-}
+};
 
 // Create the model class
-const ModelClass = mongoose.model('user', UserSchema)
+const ModelClass = mongoose.model('user', UserSchema);
 
 // Export the model
-module.exports = ModelClass
+module.exports = ModelClass;
