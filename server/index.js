11e37c4d48315db2436ff17898ac6c368a239097
Richard Eldridge <eldridge.rich@gmail.com>
2019-04-26 11:36:18 -0400

    more deployment prep

--- server/index.js
+++ server/index.js
@@ -8,6 +8,19 @@ const cors = require('cors');
 const router = require('./router');
 const keys = require('./config/keys');
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // DB Setup
 mongoose.connect(keys.MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true }, err => {
   if (err) {
